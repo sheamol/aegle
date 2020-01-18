8270c492eec135c2f1dab83668bf371796cf13b5
sheamol <msko2@cam.ac.uk>
2020-01-18 19:35:24 +0000

    Add quickstart

---
+++ quickstart-file.py
@@ -0,0 +1,11 @@
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from azure.cognitiveservices.vision.computervision.models import TextOperationStatusCodes
+from azure.cognitiveservices.vision.computervision.models import TextRecognitionMode
+from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
+from msrest.authentication import CognitiveServicesCredentials
+
+from array import array
+import os
+from PIL import Image
+import sys
+import timegit oull
